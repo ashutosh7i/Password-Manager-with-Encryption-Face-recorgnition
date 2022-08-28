1d4c7e6e38ff38db4d161281cb1816f039977a54
ashutosh7i <ashutoshsoni130@gmail.com>
2022-08-29 00:26:07 +0530

    new files

---
+++ PasswordMan-core.js
@@ -0,0 +1,5 @@
+//this is the core, console usable version of passowrdman,
+console.log("hello, I am PasswordMan, your Passowrd Manager");
+var website,username,passowrd,generate;
+var username = prompt();
+console.log(username);
